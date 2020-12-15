2f955fb26545d12c7160bad42906b3af9987b9e7
divinevinh <nxvinh222@gmail.com>
2020-12-15 23:17:32 +0700

    Add: LP model

--- src/models/test.java
+++ src/models/test.java
@@ -8,6 +8,7 @@ package models;
 import connectors.BookDbUtil;
 import connectors.CDDbUtil;
 import connectors.DVDDbUtil;
+import connectors.LPDbUtil;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -19,10 +20,12 @@ import java.util.List;
 public class test {
     public static void doTest() throws SQLException, ClassNotFoundException {
         List<Book> bookList = BookDbUtil.getAllBook();
-        System.out.print("Title: " + bookList.get(0).getTitle() + "\n");
+        System.out.print("Book Title: " + bookList.get(0).getTitle() + "\n");
         List<CD> cdList = CDDbUtil.getAllCD();
-        System.out.print("Title: " + cdList.get(0).getTitle() + "\n");
+        System.out.print("CD Title: " + cdList.get(0).getTitle() + "\n");
         List<DVD> dvdList = DVDDbUtil.getAllDVD();
-        System.out.print("Title: " + dvdList.get(0).getTitle() + "\n");
+        System.out.print("DVD Title: " + dvdList.get(0).getTitle() + "\n");
+        List<LP> lpList = LPDbUtil.getAllLP();
+        System.out.print("LP Title: " + lpList.get(0).getTitle() + "\n");
     }
 }
